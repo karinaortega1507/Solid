c0e83047056362964a6aacddac137fe00fc48d19
krortega <krortega@espol.edu.ec>
2019-11-19 16:58:55 -0500

    Se modifica la clase StoreBook

--- tallerSolid/src/tallersolid/StoreBook.java
+++ tallerSolid/src/tallersolid/StoreBook.java
@@ -7,54 +7,29 @@ package tallersolid;
 
 import java.util.ArrayList;
 
-class StoreBook implements Pay,SearchBook{
+class StoreBook implements SearchBook {
 
-	private ArrayList<Book> books;
-	private Book book;
-	
-	
-	public StoreBook() {
-		/** 
-		 * Initialize array with books
-		 */
-	}
-	
-	public void pay(String method, Book book) {
-		if(method == "card") {
-			pay_card("numberCard", book);
-		}else if ( method == "cash") {
-			pay_cash( "money", book);
-		}
-	}
-	
-		
-	public void removeBooksPurchased(ArrayList<Book> books) {
-                searchBook();
-		/*
+    private ArrayList<Book> books;
+    private Book book;
+
+    public StoreBook() {
+        /**
+         * Initialize array with books
+         */
+    }
+
+    public void removeBooksPurchased(ArrayList<Book> books) {
+        searchBook();
+        /*
 		 * store books with state true in new array and remove the original array
 		 * 
-		 */
-	}
-
-        @Override
-    public void pay_card(String card, Book book) {
-		/*
-		 * change state to true
-		 */
-	}
-	
-        @Override
-	public void pay_cash(String money, Book book) {
-		/*
-		 * change state to true
-		 */
-	}
+         */
+    }
 
     @Override
     public void searchBook() {
-		/*
+        /*
 		 * Search a book for author or title or category
-		 */
-	}
+         */
+    }
 }
-
