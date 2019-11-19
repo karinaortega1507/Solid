1ebb680474758a321c29d863e2c89b87d4da97ca
krortega <krortega@espol.edu.ec>
2019-11-19 16:58:14 -0500

    Se crea la clase Pay_card

---
+++ tallerSolid/src/tallersolid/Pay_card.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallersolid;
+
+/**
+ *
+ * @author Hp
+ */
+public class Pay_card extends Pay{
+    String card;
+    Book book;
+
+    public String getCard() {
+        return card;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+        
+
+	
+    
+}
